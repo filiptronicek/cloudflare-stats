3607e404466f1c50f72d28d3e9c7a260b39c1604
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-26 18:49:50 +0200

    Add data handeling

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ const Auth = process.env.token || require("./creds.js");
 /* Config */
 const listEndpoint = "https://api.cloudflare.com/client/v4/zones";
 const token = "Bearer " + Auth;
-
-fetch(listEndpoint, {
+const fetchConfig = {
   method: "GET",
   headers: {
     Authorization: token,
   },
-})
+};
+
+global.bandwidths = [];
+
+/* Get all the zones */
+
+fetch(listEndpoint, fetchConfig)
   .then((response) => response.json())
-  .then((resp) => console.log(resp))
+  .then((resp) => {
+    resp.result.forEach((obj) => {
+      fetch(
+        `https://api.cloudflare.com/client/v4/zones/${obj.id}/analytics/dashboard`,
+        fetchConfig
+      )
+        .then((response) => response.json())
+        .then((resp) => bandwidths.push(resp.result.totals.bandwidth.all))
+        .then(() => console.log(bandwidths.reduce((a, b) => a + b, 0)));
+    });
+  })
   .catch((err) => console.error(err));
